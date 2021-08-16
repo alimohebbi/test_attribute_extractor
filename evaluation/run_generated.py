ac304ae9cbaec62195419e89931904f17d6e0eb6
Faride Khalili <faridekhalili1393@gmail.com>
2021-08-17 01:48:15 +0430

    Choose migrator algorithm when running run_generated.py.

--- evaluation/run_generated.py
+++ evaluation/run_generated.py
@@ -1,3 +1,4 @@
+import argparse
 import os, sys
 import toml
 import glob
@@ -7,18 +8,40 @@ import test_attribute_extractor
 with open('config.toml', 'r') as file:
         config = toml.load(file)
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Evaluator')
+    parser.add_argument('--algorithm', dest='algorithm', type=str, help='migrator algorithm')
+    args = parser.parse_args()
+    if args.algorithm is None:
+        args.algorithm = "atm"
+    return args
+
+def get_file_addressed(file):
+    final_fname = "/".join(file.split("/")[0:-1])
+    log_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+ '_log.txt'])
+    final_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+".json"])
+    return final_fname, log_fname
+
 def main():
+    args = get_args()
+
     migration_configs = glob.glob(config['data']['MIGRATION_CONFIGS']['address'])
     for migration_config in migration_configs:
     	files = glob.glob(migration_config+"/*/*.java")
     	for file in files:
-            final_fname = "/".join(file.split("/")[0:-1])
-            log_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+ '_log.txt'])
-            final_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+".json"])
-            try:
-                test_attribute_extractor.ATMExtractor(file, log_fname).write_results(final_fname)
-            except Exception as e:
-                print(f"Running {file} failed with error {e}")
+            final_fname, log_fname = get_file_addressed(file)
+            if args.algorithm == "atm":
+                try:
+                    test_attribute_extractor.ATMExtractor(file, log_fname).write_results(final_fname)
+                except Exception as e:
+                    print(f"Running {file} failed with error {e}")
+            elif args.algorithm == "craftdroid":
+                try:
+                    test_attribute_extractor.CraftdroidExtractor(file, log_fname).write_results(final_fname)
+                except Exception as e:
+                    print(f"Running {file} failed with error {e}")
+            else:
+                print("Unknown algorithm: "+str(args.algorithm))
     	
     
 if __name__ == '__main__':
