606281aef73000535fb626e725f02e3649ea0cef
Faride Khalili <faridekhalili1393@gmail.com>
2021-08-20 16:13:19 +0430

    Rerun if unable to grab driver.

--- evaluation/run_generated.py
+++ evaluation/run_generated.py
@@ -22,32 +22,39 @@ def get_file_addressed(file):
     final_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+"_final.json"])
     return final_fname, log_fname
 
-def main():
 
+def run(algorithm, file, log_fname, final_fname):
+    if algorithm == "atm":
+        try:
+            test_attribute_extractor.ATMExtractor(file, log_fname).write_results(final_fname)
+        except Exception as e:
+            print(f"Running {file} failed with error {e}")
+    elif algorithm == "craftdroid":
+        try:
+            test_attribute_extractor.CraftdroidExtractor(file, log_fname).write_results(final_fname)
+        except Exception as e:
+            print(f"Running {file} failed with error {e}")
+    else:
+            print("Unknown algorithm: "+str(algorithm))
+
+def main():
     args = get_args()
     migration_configs = glob.glob(config['data']['MIGRATION_CONFIGS']['address'])
     for migration_config in migration_configs:
-        print()
-        print(migration_config)
-        print()
+        print(migration_config+'\n')
         files = glob.glob(migration_config+"/*/*.java")
         for file in files:
             final_fname, log_fname = get_file_addressed(file)
-            print()
-            print(final_fname)
-            print()
-            if args.algorithm == "atm":
-                try:
-                    test_attribute_extractor.ATMExtractor(file, log_fname).write_results(final_fname)
-                except Exception as e:
-                    print(f"Running {file} failed with error {e}")
-            elif args.algorithm == "craftdroid":
-                try:
-                    test_attribute_extractor.CraftdroidExtractor(file, log_fname).write_results(final_fname)
-                except Exception as e:
-                    print(f"Running {file} failed with error {e}")
-            else:
-                print("Unknown algorithm: "+str(args.algorithm))    
+            print(final_fname+'\n')
+            run(args.algorithm, file, log_fname, final_fname)
+            with open(log_fname) as f:
+                lines = f.readlines()
+            if len(lines) and lines[0].startswith("UNABLE TO GRAB THE DRIVER WITH CAPABILITIES"):
+                print('\n'+"RUNNING AGAIN: "+'\n')
+                run(args.algorithm, file, log_fname, final_fname)
+
+
+                
     
 if __name__ == '__main__':
     main()
