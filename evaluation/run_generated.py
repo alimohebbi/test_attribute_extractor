aa0ca18bc5a601a60ef7ec79b461078671483025
Faride Khalili <faridekhalili1393@gmail.com>
2021-08-20 18:50:32 +0430

    Prune dummy atm migrated files.

--- evaluation/run_generated.py
+++ evaluation/run_generated.py
@@ -2,6 +2,7 @@ import argparse
 import os, sys
 import toml
 import glob
+from Levenshtein import distance
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import test_attribute_extractor
 
@@ -16,12 +17,20 @@ def get_args():
         args.algorithm = "atm"
     return args
 
+def prune_files(files):
+    dummy_file_indices = set()
+    for i in range(len(files)):
+        for j in range(len(files)):
+            if distance(files[i], files[j]) == 1 and files[i][-6]!= files[j][-6]:
+                if files[i] < files[j]: 
+                    dummy_file_indices.add(i)
+    return [i for j, i in enumerate(files) if j not in dummy_file_indices]
+
 def get_file_addressed(file):
     final_fname = "/".join(file.split("/")[0:-1])
     log_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+ '_log.txt'])
     final_fname = "/".join([final_fname, file.split("/")[-1].split(".")[0]+"_final.json"])
     return final_fname, log_fname
-
 
 def run(algorithm, file, log_fname, final_fname):
     if algorithm == "atm":
@@ -43,6 +52,8 @@ def main():
     for migration_config in migration_configs:
         print(migration_config+'\n')
         files = glob.glob(migration_config+"/*/*.java")
+        if args.algorithm == "atm":
+            files = prune_files(files)
         for file in files:
             final_fname, log_fname = get_file_addressed(file)
             print(final_fname+'\n')
