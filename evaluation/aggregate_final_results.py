87a8a617f92faad563ab12bd42223e44efbffd1c
Faride Khalili <faridekhalili1393@gmail.com>
2021-10-19 11:52:20 +0330

    Fix addressings for ATM.

--- evaluation/aggregate_final_results.py
+++ evaluation/aggregate_final_results.py
@@ -23,14 +23,17 @@ def sift_erroneous_migrations():
         migrations = glob.glob(migration_config + "/*")
         for migration in migrations:
             config_migration = "/".join(migration.split("/")[-2:])
-            log_fname = glob.glob(config[ALGORITHM]['BASE_LOG_ADDRESS']['generated'] + config_migration + "/*_log.txt")[
-                0]
+            log_fname = glob.glob(config[ALGORITHM]['BASE_LOG_ADDRESS']['generated'] + config_migration + "/*_log.txt")[0]
+            final_fname = glob.glob(config[ALGORITHM]['BASE_JSON_ADDRESS']['generated'] + config_migration + "/*_final.json")[0]
             with open(log_fname) as f:
                 lines = f.readlines()
             if len(lines):
-                erroneous_address = erroneous_generated + "/" + '/'.join(migration.split("/")[-2:])
+                erroneous_address = erroneous_generated + '/'.join(migration.split("/")[-2:])
+                print(erroneous_address)
                 if not os.path.exists(erroneous_address):
                     shutil.copytree(migration, erroneous_address)
+                shutil.copyfile(log_fname, erroneous_address+"/"+log_fname.split("/")[-1])
+                shutil.copyfile(final_fname, erroneous_address+"/"+final_fname.split("/")[-1])
 
 
 def aggregate_results():
@@ -39,16 +42,14 @@ def aggregate_results():
     number_of_errors_list = []
     number_of_blocking_errors_list = []
     erroneous_generated = config[ALGORITHM]['MIGRATION_CONFIGS']['erroneous_address']
-    migration_configs = glob.glob(config[ALGORITHM]['MIGRATION_CONFIGS']['address'])
-
+    migration_configs = glob.glob(config[ALGORITHM]['BASE_JSON_ADDRESS']['generated']+"/*")
     for migration_config in migration_configs:
         config_list.append(migration_config.split("/")[-1])
 
         number_of_migrations_list.append(len(glob.glob(migration_config + "/*")))
 
-        erroneous_migrations = glob.glob(erroneous_generated + "/" + migration_config.split("/")[-1] + "/*")
+        erroneous_migrations = glob.glob(erroneous_generated+ migration_config.split("/")[-1] + "/*")
         number_of_errors_list.append(len(erroneous_migrations))
-
         num_blocking_errors = 0
         for migration in erroneous_migrations:
             if load_json_data(glob.glob(migration + "/*_final.json")[0]) is None:
